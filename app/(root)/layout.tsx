dd8f766a5027a4741510a08008c47d90d21e7e86
Muglesh <mugibabu8@gmail.com>
2025-04-19 13:13:45 +0530

    changes in globals.css

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,7 @@ import {redirect} from "next/navigation";
 const RootLayout = async ({children}: { children: ReactNode }) => {
     const isUserAuthenticated = await isAuthenticated();
 
-    if (!isUserAuthenticated) redirect('/sign-in')
+    if (!isUserAuthenticated) redirect('/sign-in');
     return (
         <div className="root-layout">
             <nav>
