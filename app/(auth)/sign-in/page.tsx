cafccf5c99c3b4dba15aeb7b7cb9c82d17c2856a
Muglesh <mugibabu8@gmail.com>
2025-04-19 12:12:40 +0530

    all static work done

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AuthLayout from '@/components/authForm'
+const Page = () => {
+    return (
+        <AuthLayout type = "sign-in"/>
+    )
+}
+export default Page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuthForm from "@/components/authForm";
+
+const Page = () => {
+    return (
+        <AuthForm type = "sign-up"/>
+    )
+}
+export default Page
